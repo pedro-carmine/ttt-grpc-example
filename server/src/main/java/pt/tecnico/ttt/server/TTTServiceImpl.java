aa8229c894944c47cc2cecede4b151cb00b30e10
Pedro Carmine <pedro.carmine@tecnico.ulisboa.pt>
2022-03-15 16:23:29 +0000

    init commit

---
+++ server/src/main/java/pt/tecnico/ttt/server/TTTServiceImpl.java
@@ -0,0 +1,50 @@
+package pt.tecnico.ttt.server;
+
+import io.grpc.stub.StreamObserver;
+import pt.tecnico.ttt.*;
+
+public class TTTServiceImpl extends TTTGrpc.TTTImplBase {
+
+	/** Game implementation. */
+	private TTTGame ttt = new TTTGame();
+
+	@Override
+	public void currentBoard(CurrentBoardRequest request, StreamObserver<CurrentBoardResponse> responseObserver) {
+		// StreamObserver is used to represent the gRPC stream between the server and
+		// client in order to send the appropriate responses (or errors, if any occur).
+
+		CurrentBoardResponse response = CurrentBoardResponse.newBuilder().setBoard(ttt.toString()).build();
+
+		// Send a single response through the stream.
+		responseObserver.onNext(response);
+		// Notify the client that the operation has been completed.
+		responseObserver.onCompleted();
+	}
+
+    @Override
+    public void play(PlayRequest request, StreamObserver<PlayResponse> responseObserver) {
+        // StreamObserver is used to represent the gRPC stream between the server and
+        // client in order to send the appropriate responses (or errors, if any occur).
+
+        PlayResponse response = PlayResponse.newBuilder().setResponse(ttt.play(request.getRow(), request.getColumn(), request.getPlayer())).build();
+
+        // Send a single response through the stream.
+        responseObserver.onNext(response);
+        // Notify the client that the operation has been completed.
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void checkWinner(CheckWinnerRequest request, StreamObserver<CheckWinnerResponse> responseObserver) {
+        // StreamObserver is used to represent the gRPC stream between the server and
+        // client in order to send the appropriate responses (or errors, if any occur).
+
+        CheckWinnerResponse response = CheckWinnerResponse.newBuilder().setResult(ttt.checkWinner()).build();
+
+        // Send a single response through the stream.
+        responseObserver.onNext(response);
+        // Notify the client that the operation has been completed.
+        responseObserver.onCompleted();
+    }
+
+}
